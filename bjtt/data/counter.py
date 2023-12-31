398bfcbbe87e24785b7b534d1972479f7b72b886
Chayan Toufan Tabrizi <chayantt@gmail.com>
2023-12-31 18:14:38 +0100

    refactor: :lock: n_cards in counter object

--- bjtt/data/counter.py
+++ bjtt/data/counter.py
@@ -7,6 +7,7 @@ class Counter:
         self.running_divisor: float = 0
         self.true_count: float = 0
         self.n_decks: int = n_decks
+        self.n_cards = 0
 
     def reset(self) -> None:
         self.count = 0
@@ -20,10 +21,11 @@ class Counter:
         else:
             self.count -= 1
 
-    def update_running_divisor(self, n_cards: int) -> None:
-        self.running_divisor = (52 * self.n_decks - n_cards) / 52
+    def update_running_divisor(self) -> None:
+        self.running_divisor = (52 * self.n_decks - self.n_cards) / 52
 
-    def update(self, card: Card, n_cards: int) -> None:
+    def update(self, card: Card) -> None:
+        self.n_cards += 1
         self.update_count(card)
-        self.update_running_divisor(n_cards)
+        self.update_running_divisor()
         self.true_count = self.count / self.running_divisor
--- bjtt/data/test_counter.py
+++ bjtt/data/test_counter.py
@@ -9,28 +9,28 @@ def test_counter():
     assert counter.count == 0
     assert counter.true_count == 0
     assert counter.n_decks == 4
-    counter.update(Card("A", "H"), 1)
+    counter.update(Card("A", "H"))
     assert counter.count == -1
     assert counter.true_count == approx(-0.2512, 1e-4)
-    counter.update(Card("2", "H"), 2)
+    counter.update(Card("2", "H"))
     assert counter.count == 0
     assert counter.true_count == approx(0, 1e-4)
-    counter.update(Card("3", "H"), 3)
-    counter.update(Card("4", "H"), 4)
-    counter.update(Card("5", "H"), 5)
-    counter.update(Card("6", "H"), 6)
+    counter.update(Card("3", "H"))
+    counter.update(Card("4", "H"))
+    counter.update(Card("5", "H"))
+    counter.update(Card("6", "H"))
     assert counter.count == 4
     assert counter.true_count == approx(1.0297, 1e-4)
     counter.reset()
     assert counter.count == 0
     assert counter.true_count == 0
-    counter.update(Card("7", "H"), 1)
-    counter.update(Card("8", "H"), 2)
+    counter.update(Card("7", "H"))
+    counter.update(Card("8", "H"))
     assert counter.count == 0
     assert counter.true_count == 0
-    counter.update(Card("A", "H"), 1)
-    counter.update(Card("J", "H"), 2)
-    counter.update(Card("Q", "H"), 3)
-    counter.update(Card("K", "H"), 4)
+    counter.update(Card("A", "H"))
+    counter.update(Card("J", "H"))
+    counter.update(Card("Q", "H"))
+    counter.update(Card("K", "H"))
     assert counter.count == -4
     assert counter.true_count == approx(-1.0196, 1e-4)
